da2a8017fc345b794afee61362a806e63290b166
Luzu Shopping <63834302+luzuvn@users.noreply.github.com>
2020-04-27 18:46:09 +0700

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,46 @@
+/**
+ * script for each single page.
+ *
+ */
+(function() {
+    "use strict";
+
+    var imported = document.createElement('script');
+    imported.src = 'https://luzu.vn/asset/js/owl.carousel.min.js';
+    document.head.appendChild(imported);
+
+    // var owl = document.getElementById('owl-demo');
+    // 	for (var i = 0; i < 15; i++) {
+    // 		var item = document.createElement('div');
+    // 			item.classList.add('item');
+    // 			owl.appendChild(item);
+    // 			var h1 = document.createElement('h1');
+    // 				ht.innerHTMLin = i;
+
+    // 	}
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//--
